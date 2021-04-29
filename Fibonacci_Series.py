fdcccca34e505d6c63565bcbed1b72438a694c97
Sathish Kumar keshavarapu <74714002+54TH15H@users.noreply.github.com>
2021-04-29 23:04:50 +0530

    Fibonacci_Series

---
+++ Fibonacci_Series.py
@@ -0,0 +1,26 @@
+def check(n):
+    try:
+        if int(n) < 0:
+            raise ValueError
+        else:
+            return int(n)
+    except ValueError:
+        print("Invalid input")
+        return check(input("Plz, Enter a Valid Number : ))
+
+print(" Fibonacci Series ")
+a, b=0, 1
+num = check(input("Enter a Positive Number : "))
+if num == 0:
+    print("   ")
+elif num == 1:
+    print("0")
+elif num == 2:
+    print("0  1")
+else:
+    print(a, end = "  ")
+    print(b, end = "  ")
+    for i in range(2, num):
+        c = a+b
+        print(c, end = "  ")
+        a, b = b, c
